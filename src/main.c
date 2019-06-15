14919416c2f3d32f565cffe703b9fb66b50933c0
Luke Campbell <luke.s.campbell@gmail.com>
2019-06-15 15:33:58 -0400

    Ready to build

--- src/main.c
+++ src/main.c
@@ -3,11 +3,14 @@
  */
 
 #include <stdio.h>
+#include <math.h>
 #include "sum.h"
 
 int main(int argc, char *argv[]) {
     double a = 2.718;
     double b = 5.2;
-    printf("%f\n", sum(a, b));
+    if (fabs(sum(a,b) - (a + b)) > 0.01) {
+        return 1;
+    }
     return 0;
 }
